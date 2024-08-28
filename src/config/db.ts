8d1f409d7064c03602fc513f3be7b0a490df70b0
Vitalii Polishchuk <vetal.polishuk@gmail.com>
2024-08-28 15:05:06 +0300

    do not reject ssl unauthorized

--- src/config/db.ts
+++ src/config/db.ts
@@ -9,6 +9,9 @@ const pool = new Pool({
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: 5432,  // Default PostgreSQL port
+    ssl: {
+        rejectUnauthorized: false // Render uses self-signed certificates
+    }
 });
 
 export default pool;
